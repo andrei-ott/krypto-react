eb8b7027bfa442f3b2d505f1b4e4078b462deaf7
andrei-ott <ott.andrey.ao@gmail.com>
2025-04-05 17:20:03 +0400

    add featured on

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,3 +1,7 @@
+import fastCompany from 'src/assets/featured/fast-company.png';
+import forbes from 'src/assets/featured/forbes.png';
+import mit from 'src/assets/featured/mit.png';
+import techCrunch from 'src/assets/featured/tech-crunch.png';
 import heroMonitor from 'src/assets/hero-monitor.png';
 import MainButton from 'src/components/MainButton/MainButton.tsx';
 import { MainButtonType } from 'src/types/enums.ts';
@@ -21,6 +25,17 @@ function Home() {
 
         <img src={heroMonitor} alt="" width={552} height={586} />
       </article>
+
+      <section className="mt-23">
+        <div className="sub-header mb-7">Featured on</div>
+
+        <div className="flex py-3 px-16 justify-between items-center bg-(--dark-purple) rounded-[7px]">
+          <img src={techCrunch} alt="Tech Crunch" width={207} height={77} />
+          <img src={fastCompany} alt="Tech Crunch" width={184} height={28} />
+          <img src={mit} alt="Tech Crunch" width={74} height={40} />
+          <img src={forbes} alt="Tech Crunch" width={126} height={32} />
+        </div>
+      </section>
     </div>
   );
 }
